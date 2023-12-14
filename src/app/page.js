50023120766657aa69f104d1e6f49182d49b219f
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-15 01:17:30 +0300

    add weather component to main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
 import Image from 'next/image'
+import Weather from './components/Weather'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello Next</h1>
+      <Weather />
     </main>
   )
 }
