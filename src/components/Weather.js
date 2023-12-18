7e444b499d31a2cbaf15ce5f4759deb1d8bc8f98
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-18 15:56:29 +0300

    add icon to page

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react'
 import axios from 'axios';
+import { CiSearch } from "react-icons/ci";
 
 function Weather() {
     const [loading, setLoading] = useState(false);
@@ -28,6 +29,36 @@ function Weather() {
                 onChange={(e) => setCity(e.target.value)}
           />
           <button onClick={getWeather}>Get Weather</button>
+
+          <div className="weather-card">
+              <div className="search">
+                  <input type="search" placeholder="enter city name" spellcheck="false" />
+                  <button className='flex justify-center items-center'>
+                      <CiSearch className='text-[20px] text-gray-500'/>
+                  </button>
+              </div>
+              <div className="weather">
+                  <img className="weather-icon" src="https://static.vecteezy.com/system/resources/previews/024/825/182/non_2x/3d-weather-icon-day-with-rain-free-png.png" alt="..." />
+                  <h1 className="temp">15°C </h1>
+                  <h2 className="city">New York</h2>
+                  <div className="details">
+                      <div style={{ "display": "flex" }} classNameName="col">
+                          <img className="humi" src="https://static-00.iconduck.com/assets.00/humidity-icon-2048x1675-xxsge5os.png" />
+                          <div className="info">
+                              <p className="humidity">50%</p>
+                              <p>Humidity</p>
+                          </div>
+                      </div>
+                      <div className="col">
+                          <img src="https://cdn-icons-png.flaticon.com/512/136/136712.png" />
+                          <div className="info">
+                              <p className="wind">15 km/h</p>
+                              <p>Wind Speed</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
           
           {loading && <p>Loading...</p>}
           
@@ -36,36 +67,6 @@ function Weather() {
                     <h1>{weatherData.name}</h1>
                   <h2>{weatherData.weather[0].main.temp}°C</h2>
                   <h2>{weatherData.weather[0].description}</h2>
-                  
-                  <div class="weather-card">
-                      <div class="search">
-                          <input type="search" placeholder="enter city name" spellcheck="false"/>
-                              <button>
-                                  <i class="bi bi-search"></i>
-                              </button>
-                      </div>
-                      <div class="weather">
-                          <img class="weather-icon" src="https://static.vecteezy.com/system/resources/previews/024/825/182/non_2x/3d-weather-icon-day-with-rain-free-png.png" alt="..."/>
-                              <h1 class="temp">15°C </h1>
-                              <h2 class="city">New York</h2>
-                              <div class="details">
-                                  <div style="display: flex;" class="col">
-                                      <img class="humi" src="https://static-00.iconduck.com/assets.00/humidity-icon-2048x1675-xxsge5os.png"/>
-                                          <div class="info">
-                                              <p class="humidity">50%</p>
-                                              <p>Humidity</p>
-                                          </div>
-                                  </div>
-                                  <div class="col">
-                                      <img src="https://cdn-icons-png.flaticon.com/512/136/136712.png"/>
-                                          <div class="info">
-                                              <p class="wind">15 km/h</p>
-                                              <p>Wind Speed</p>
-                                          </div>
-                                  </div>
-                              </div>
-                      </div>
-                  </div>
                 </div>
             )}
     </>
